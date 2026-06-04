49f9c652ddec50ca9e7c312472f2d979d9d3ae21
Fluffy <blakersthesnake@gmail.com>
2026-06-04 18:21:53 -0500

    so god damn hard...

---
+++ entities/entities/flgm_anomaly_rendertest.lua
@@ -0,0 +1,33 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+
+ENT.PrintName = "Render Cube"
+ENT.Author = "ChatGPT"
+ENT.Category = "Fluffy's gamemode"
+ENT.Spawnable = true
+
+
+function ENT:Initialize()
+    if SERVER then
+        self:SetModel("models/hunter/blocks/cube05x05x05.mdl")
+
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetMoveType(MOVETYPE_VPHYSICS)
+        self:SetSolid(SOLID_VPHYSICS)
+        self.Draw = false
+        self:SetUseType(SIMPLE_USE)
+
+        self:SetNWBool("RenderCube", false)
+    end
+end
+
+function ENT:Use(Activator, Caller)
+    if SERVER then
+        local env_sun = ents.FindByClass("env_sun")
+        for i,v in pairs(env_sun) then
+            v:SetNoDraw(true)
+        end
+    end
+end
