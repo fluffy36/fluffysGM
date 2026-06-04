8a8580f30238a8aba03c108f2f1fd7acc255f95e
Fluffy <blakersthesnake@gmail.com>
2026-06-03 21:31:48 -0500

    s

--- gamemode/Events.lua
+++ gamemode/Events.lua
@@ -184,7 +184,6 @@ hook.Add("PlayerInitialSpawn", "KillFirstJoinOnce", function(Ply)
                     prop:SetPos(RandPoint + Vector(0, 0, 70)) -- Drop from the sky
                     prop:Spawn()
                     local Physicsobj = prop:GetPhysicsObject()
-                    print(IsValdid(Physicsobj))
                     if IsValid(Physicsobj) then
                         Physicsobj:Wake()
                         Physicsobj:ApplyForceCenter(Vector(0, 0, -10))
