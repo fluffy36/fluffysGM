80c92f10a2b0b53a46c3cb5e51ec2903f1d898fd
Fluffy <blakersthesnake@gmail.com>
2026-06-03 20:57:12 -0500

    crap

--- entities/weapons/flgm_tool.lua
+++ entities/weapons/flgm_tool.lua
@@ -133,7 +133,7 @@ function SWEP:PrimaryAttack()
 				end
 			end
 		end
-    elseif trace.Entity:IsWorld() then
+    elseif !trace.Entity:IsWorld() then
 		trace.Entity:EmitSound("friends/message.wav",0)
 		if ( SERVER ) then
 			trace.Entity:Remove()
