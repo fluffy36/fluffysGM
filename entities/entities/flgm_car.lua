ef3f39895995d995b0fa7f6625512b39e53bf9a9
Lennard <kcem001@gmail.com>
2026-06-11 15:06:44 +0300

    updated the inbox car

--- entities/entities/flgm_car.lua
+++ entities/entities/flgm_car.lua
@@ -5,8 +5,12 @@ ENT.Base = "base_anim"
 ENT.PrintName = "Inbox Car"
 ENT.Category = "Fluffy's gamemode"
 ENT.Spawnable = true
-
+if CLIENT then
+    killicon.Add("inbox_car", "HUD/killicons/default", Color(255, 0, 0, 255))
+    -- too lazy to add
+end
 function ENT:Initialize()
+    
     self:SetModel("models/props_vehicles/car004a_physics.mdl")
     self:PhysicsInit(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS)
@@ -16,11 +20,10 @@ function ENT:Initialize()
     local phys = self:GetPhysicsObject()
     if IsValid(phys) then
         phys:Wake()
-        phys:SetMass(50000)
-        self.mass = self:GetPhysicsObject():GetMass()
+        phys:SetMass(5000000) 
+        self.mass = phys:GetMass()
     end
     
-    -- Driver seat
     self.Seat = ents.Create("prop_vehicle_prisoner_pod")
     self.Seat:SetModel("models/nova/airboat_seat.mdl")
     self.Seat:SetPos(self:LocalToWorld(Vector(5,15,-10)))
@@ -28,7 +31,6 @@ function ENT:Initialize()
     self.Seat:Spawn()
     self.Seat:SetParent(self)
 
-    -- Passenger seat
     self.Seat2 = ents.Create("prop_vehicle_prisoner_pod")
     self.Seat2:SetModel("models/nova/airboat_seat.mdl")
     self.Seat2:SetPos(self:LocalToWorld(Vector(5,-15,-10)))
@@ -38,61 +40,51 @@ function ENT:Initialize()
 
     self.EngineSound = CreateSound(self, "vehicles/v8/first.wav")
     self.EngineSound:PlayEx(1, 50)
-
-    self.NextThink = CurTime()
-    self.PassengerisAvailable=false
-    self.Passanger = nil
-    self.Driver = nil
-
-    print(list.Get("Vehicles"))
+    self.PassengerisAvailable = false
 end
 
-function ENT:Use(activator, caller, useType, value)
-    if !self.PassengerisAvailable then
+function ENT:Use(activator, caller)
+    if not self.PassengerisAvailable then
         activator:EnterVehicle(self.Seat)
-        self.PassengerisAvailable=true
-    elseif self.PassengerisAvailable and !IsValid(self.Seat2:GetDriver()) then
+        self.PassengerisAvailable = true
+    elseif self.PassengerisAvailable and IsValid(self.Seat2) and not IsValid(self.Seat2:GetDriver()) then
         activator:EnterVehicle(self.Seat2)
-        self.Passanger = activator
     end
 end
 
 function ENT:Think()
-
     local phys = self:GetPhysicsObject()
     if not IsValid(phys) then return end
-
     local driver = IsValid(self.Seat) and self.Seat:GetDriver()
 
     if IsValid(driver) then
+        -- stability
+        local ang = self:GetAngles()
+        local angVel = phys:GetAngleVelocity()
+        phys:AddAngleVelocity(Vector(-ang.p * 0.5 - angVel.x * 0.1, -ang.r * 0.5 - angVel.y * 0.1, 0))
 
         local turbo = driver:KeyDown(IN_SPEED) and 2 or 1
+        local moveForce = 500000 * turbo
 
         if driver:KeyDown(IN_FORWARD) then
-            phys:ApplyForceCenter(self:GetForward() * 50000 * turbo * self.mass)
-        end
-
-        if driver:KeyDown(IN_BACK) then
-            phys:ApplyForceCenter(-self:GetForward() * 50000 * turbo * self.mass)
+            phys:ApplyForceCenter(self:GetForward() * moveForce)
+        elseif driver:KeyDown(IN_BACK) then
+            phys:ApplyForceCenter(-self:GetForward() * moveForce)
         end
 
         if driver:KeyDown(IN_MOVELEFT) then
-            phys:AddAngleVelocity(Vector(0,0,100))
-        end
-
-        if driver:KeyDown(IN_MOVERIGHT) then
-            phys:AddAngleVelocity(Vector(0,0,-100))
+            phys:AddAngleVelocity(Vector(0,0,10))
+        elseif driver:KeyDown(IN_MOVERIGHT) then
+            phys:AddAngleVelocity(Vector(0,0,-10))
         end
 
         if driver:KeyPressed(IN_JUMP) then
-            phys:ApplyForceCenter(Vector(0,0,750000))
+            phys:ApplyForceCenter(Vector(0, 0, 9990000))
             self:EmitSound("physics/metal/metal_barrel_impact_hard1.wav")
         end
     else
-        if IsValid(self.Seat2:GetDriver()) then
-            local driver = self.Seat2:GetDriver()
-
-            driver:EnterVehicle(self.Seat)
+        if IsValid(self.Seat2) and IsValid(self.Seat2:GetDriver()) then
+            self.Seat2:GetDriver():EnterVehicle(self.Seat)
             self.PassengerisAvailable = true
         else
             self.PassengerisAvailable = false
@@ -100,13 +92,31 @@ function ENT:Think()
     end
 
     local speed = phys:GetVelocity():Length()
-
     if self.EngineSound then
         self.EngineSound:ChangePitch(math.Clamp(speed / 6, 30, 140), 0.1)
     end
-
     self:NextThink(CurTime())
     return true
+end
+
+function ENT:PhysicsCollide(data, phys)
+    local energy = data.Speed
+
+    -- mr booger known as fluffy can fix this i fail
+    if energy > 600 then
+        local sparkScale = math.Clamp(energy / 400, 2, 10)
+        local effectdata = EffectData()
+        effectdata:SetOrigin(data.HitPos)
+        effectdata:SetNormal(data.HitNormal)
+        effectdata:SetMagnitude(sparkScale)
+        effectdata:SetScale(sparkScale)
+        util.Effect("Sparks", effectdata, true)
+
+    elseif energy > 200 then
+        local effectdata = EffectData()
+        effectdata:SetOrigin(data.HitPos)
+        util.Effect("cball_explode", effectdata, true)
+    end
 end
 
 function ENT:PhysicsCollide(data, phys)
@@ -131,7 +141,7 @@ function ENT:PhysicsCollide(data, phys)
 end
 
 function ENT:OnRemove()
-    if self.EngineSound then
-        self.EngineSound:Stop()
-    end
+    if self.EngineSound then self.EngineSound:Stop() end
+    if IsValid(self.Seat) then self.Seat:Remove() end
+    if IsValid(self.Seat2) then self.Seat2:Remove() end
 end
